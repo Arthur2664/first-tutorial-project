947edd36ab1c3ad16dd4578d8cca36b643a7dc64
Artur Yakymchuk <Yakymchuk.Artur@formpipe.com>
2024-09-02 16:40:53 +0300

    migrate to .net scripting

---
+++ objects/coin/Coin.cs
@@ -0,0 +1,25 @@
+using Godot;
+
+public partial class Coin : Area3D
+{
+	private const float RotationSpeed = 5.0f;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		float degreeWithTime = (float)(RotationSpeed * delta);
+
+		RotateY(degreeWithTime);
+	}
+
+#pragma warning disable IDE0060 // Remove unused parameter
+
+    private void HadleBodyEntered(Node3D node3D) => QueueFree();
+#pragma warning restore IDE0060 // Remove unused parameter
+
+}
