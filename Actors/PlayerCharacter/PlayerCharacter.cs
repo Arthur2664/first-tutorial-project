7ef5ad896f80bbfb4f46b8b2a01a7f98773a5fea
Artur Yakymchuk <Yakymchuk.Artur@formpipe.com>
2024-09-15 16:46:54 +0300

    Make character align with the floor

--- Actors/PlayerCharacter/PlayerCharacter.cs
+++ Actors/PlayerCharacter/PlayerCharacter.cs
@@ -2,17 +2,27 @@ using Godot;
 
 public partial class PlayerCharacter : CharacterBody3D
 {
-	private const float Speed = 5.0f;
-	private const float JumpVelocity = 4.5f;
+	private Transform3D xForm;
+
+	[Export]
+	public float Speed = 5.0f;
+
+	[Export]
+	public float JumpVelocity = 4.5f;
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector3 velocity = Velocity;
 
 		// Handle Jump.
+		if(IsOnFloor())
+		{
+			this.AlignWithFloor(this.GetNode<RayCast3D>("FloorAlignRay").GetCollisionNormal());
+			this.Transform = this.Transform.InterpolateWith(this.xForm, 0.3f);
+		}
 		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
 		{
-			velocity.Y = JumpVelocity;
+			velocity.Y = this.JumpVelocity;
 		}
 
 		// Get the input direction and handle the movement/deceleration.
@@ -22,16 +32,24 @@ public partial class PlayerCharacter : CharacterBody3D
 		
 		if (direction != Vector3.Zero)
 		{
-			velocity.X = direction.X * Speed;
-			velocity.Z = direction.Z * Speed;
+			velocity.X = direction.X * this.Speed;
+			velocity.Z = direction.Z * this.Speed;
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, this.Speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, this.Speed);
 		}
 
 		Velocity = velocity;
 		MoveAndSlide();
+	}
+
+	private void AlignWithFloor(Vector3 collisionNormal)
+	{
+		this.xForm = this.Transform;
+		this.xForm.Basis.Y = collisionNormal;
+		this.xForm.Basis.X = -this.xForm.Basis.Z.Cross(collisionNormal);
+		this.xForm.Basis = this.xForm.Basis.Orthonormalized();
 	}
 }
